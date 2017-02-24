03fdc62d91ec9caba2b53853637f402bf199ffa0
Isaac <isaac@nicofun.club>
2017-02-25 00:08:46 +0900

    Modify auth url

--- myauth/urls.py
+++ myauth/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from django.contrib.auth.views import logout
 
 urlpatterns = [
-    url(r'^twitter', include('social_django.urls', namespace='social')),
+    url(r'^', include('social_django.urls', namespace='social')),
     url(r'^logout', logout, name='logout'),
 ]
