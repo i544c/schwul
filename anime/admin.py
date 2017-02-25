b824c0cc3640b0164a878c3eab63239ed14f4b95
Isaac <r.sato7690@me.com>
2017-02-26 04:30:58 +0900

    Add anime model

--- anime/admin.py
+++ anime/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Anime, Story
+
 # Register your models here.
+class StoryInline(admin.StackedInline):
+    model = Story
+    extra = 3
+
+class AnimeAdmin(admin.ModelAdmin):
+    field = ['title', 'bc_year', 'synopsis']
+    inlines = [StoryInline]
+
+admin.site.register(Anime, AnimeAdmin)
--- myauth/urls.py
+++ myauth/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib.auth.views import logout
 
+from . import views
+
 app_name = 'myauth'
 urlpatterns = [
+    url(r'^login', views.login, name='login'),
     url(r'^logout', logout, name='logout'),
 ]
