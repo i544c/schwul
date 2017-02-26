c0ba33d78f4ca693c058d08ac384055149829e9a
Isaac <r.sato7690@me.com>
2017-02-27 02:29:57 +0900

    Add anime description

--- anime/urls.py
+++ anime/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'anime'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<title>[\s\S]+)/$', views.detail, name='detail')
 ]
--- anime/views.py
+++ anime/views.py
@@ -1,7 +1,26 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render
+
+from .models import Anime
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'anime/index.html')
+    anime_list = Anime.objects.all()
+    context = {
+        'anime_list': anime_list,
+    }
+    return render(request, 'anime/index.html', context)
+
+
+def detail(request, title):
+    try:
+        anime = Anime.objects.get(title=title)
+    except Anime.DoesNotExist:
+        raise Http404("そのアニメは存在しません...")
+
+    context = {
+        'anime': anime
+    }
+    return render(request, 'anime/detail.html', context)
