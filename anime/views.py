12a36bce841d536d40fef95c2a8307c575f2c530
Isaac <r.sato7690@me.com>
2017-03-05 04:02:23 +0900

    Add anime add function

--- anime/views.py
+++ anime/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Anime
 
@@ -16,11 +16,15 @@ def index(request):
 
 def add(request):
     if request.POST:
-        context = [
-            request.POST['title'],
-            request.POST['synopsis'],
-        ]
-        return HttpResponse(context)
+        title = request.POST['title']
+        bc_year = request.POST['bc_year']
+        synopsis = request.POST['synopsis']
+        if title and bc_year and synopsis:
+            anime = Anime(title=title, bc_year=bc_year, synopsis=synopsis)
+            anime.save()
+            return render(request, 'anime/add.html', {
+                'message': "追加"
+            })
 
     return render(request, 'anime/add.html')
 
